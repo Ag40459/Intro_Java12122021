a78d46e50ac0dede2d7653238ac1cbc0e6352539
Agenor <agenortorres10@gmail.com>
2021-12-13 20:11:36 -0300

    Exckui o "Seja bem Vindo" no começo da impressao de texto no console
    criei o metodo modo mais curto e movi o codigo que estava no main
    para lá
    
    o metodo mais agora chama os 2 metodos calcular que passaram tambem a se estaticos

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -5,46 +5,56 @@ package intro;
 
 // 3 - Classe
 
-public class Medidas {
-        // 3.1 - Atributo - Caracteristicas
+             public class Medidas {
+                                              // 3.1 - Atributo - Caracteristicas
+                                             // 3.2 - Metodos e funções ( o que ele sabe fazer) e (faz alguma coisa)
 
-        // 3.2 - Metodos e funções ( o que ele sabe fazer) e (faz alguma coisa)
+             public static void main (String[] args) {
 
-         public static void main (String[] args) {
-             System.out.println("SEJA BEM VINDO!!!");
-             System.out.println("Meu primeiro codigo em java");
+                    calculodeareamodocurto();
+                    calculodedeareamodolongo();
 
-             // CALCULO DE AREA FORMULA MAIS LONGA
+             }
+                                            // INICIO CALCULO DE AREA - MODO CURTO
 
+             public static void calculodeareamodocurto(){
+
+                 System.out.println("CALCULO DE AREA MODO CURTO");
+
+
+
+                 int largura = 4;           //LARGURA RECEBE 4
+                 int comprimento = 3;       //COMPRIMENTO RECEBE 3
+
+                 System.out.println("PARA LARGURA DE " + largura + "m E O COMPRIMENTO DE "
+                         + comprimento + "m A AREA É DE " + largura * comprimento + "m²");
+
+
+             }
+                                        // FINAL CALCULO DE AREA - MODO CURTO
+
+
+                                        // INICIO CALCULO DE AREA MODO LONGO
+
+             public static void calculodedeareamodolongo(){
+
+                 System.out.println("CALCULO DE AREA MODO LONGO");
              int largura;
              int comprimento;
              int resultado;
 
 
-             largura = 8;        //LARGURA RECEBE 8
-             comprimento = 3;   //COMPRIMENTO RECEBE 3
+             largura = 8;                   //LARGURA RECEBE 8
+             comprimento = 3;               //COMPRIMENTO RECEBE 3
 
              resultado = largura * comprimento;
 
              System.out.println("PARA LARGURA DE " + largura + "m E O COMPRIMENTO DE "
                      + comprimento + "m A AREA É DE " + resultado + "m²");
 
+
          }
-
-                // CALCULO DE AREA - FORMULA REDUZIDA
-
-                public void calcularAreaFormulaCompacta(){
-
-                    int largura = 4;
-                    int comprimento = 8;
-
-                    System.out.println("PARA LARGURA DE " + largura + "m E O COMPRIMENTO DE "
-                            + comprimento + "m A AREA É DE " + largura * comprimento + "m²");
-
-
-             }
-
-
+                                            // FINAL CALCULO DE AREA MODO LONGO
 
 
 
